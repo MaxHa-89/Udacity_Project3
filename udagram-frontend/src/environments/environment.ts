fcefbbc1216f3b05df5c51b762baacbd4dc955dd
MaxHa-89 <maximilian.halter89@gmail.com>
2021-02-24 14:12:53 +0000

    Letzte Änderung

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab5770ecb9aa74ed79328b5ac0914440-796047553.eu-central-1.elb.amazonaws.com/api/v0'
 };
 
 /*
